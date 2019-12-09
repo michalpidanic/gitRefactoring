7797dbfd6fe30a324a2408ff6ec7c7fb76bdb8a8
Michal Pidanic <michal.pidanic1@gmail.com>
2019-12-09 23:41:47 +0100

    function n

--- projekt/main.c
+++ projekt/main.c
@@ -1,9 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define filePath "/Users/macbook/Documents/University/MIP/gitRefactoring/projekt/reality.txt"
 
-void n(){
-    printf("Nacita zaznamy\n");
+typedef struct flat {
+    char category[51];
+    char place[51];
+    char street[101];
+    int area;
+    int price;
+    char description[201];
+    struct flat *link;
+}FLAT;
+
+struct flat *n(int *pCounter){
+    struct flat *root = NULL;
+    struct flat *actual = NULL;
+    struct flat *helper = NULL;
+    FILE *file;
+    int counter = 0, areaRead, priceRead;
+    char trash[5];
+    char categoryRead[51];
+    char placeRead[51];
+    char streetRead[101];
+    char descriptionRead[201];
+
+    if ((file = fopen(filePath, "r")) != NULL){
+        while(!feof(file)){
+            if(root == NULL){
+                root = (struct flat *)malloc(sizeof(struct flat));
+                fgets(trash, 5, file);
+                strcpy(root->category, fgets(categoryRead, 51, file));
+                strcpy(root->place, fgets(placeRead, 51, file));
+                strcpy(root->street, fgets(streetRead, 101, file));
+                fscanf(file, "%d\n", &areaRead);
+                root->area = areaRead;
+                fscanf(file, "%d\n", &priceRead);
+                root->price = priceRead;
+                strcpy(root->description, fgets(descriptionRead, 201, file));
+                root->link = NULL;
+                actual = root;
+            } else{
+                helper = (struct flat *)malloc(sizeof(struct flat));
+                fgets(trash, 5, file);
+                strcpy(helper->category, fgets(categoryRead, 51, file));
+                strcpy(helper->place, fgets(placeRead, 51, file));
+                strcpy(helper->street, fgets(streetRead, 101, file));
+                fscanf(file, "%d\n", &areaRead);
+                helper->area = areaRead;
+                fscanf(file, "%d\n", &priceRead);
+                helper->price = priceRead;
+                strcpy(helper->description, fgets(descriptionRead, 201, file));
+                helper->link = NULL;
+                actual->link = helper;
+                actual = actual->link;
+            }
+            counter++;
+        }
+    } else{
+        printf("Zaznamy neboli nacitane\n");
+        exit(1);
+    }
+
+    printf("Nacitalo sa %d zaznamov\n", counter);
+    *pCounter = counter;
+    return root;
 }
 
 void v(){
@@ -29,13 +90,15 @@ void a(){
 int main() {
     int bool = 1;
     char function;
+    struct flat *root = NULL;
+    int counter = 0;
 
     while(bool == 1){
         scanf("%c", &function);
 
         switch (function) { //znak zo vstupu je zistovany pomocou switch, na zaklade nacitaneho znaku sa vykona jedna z funkcii vyssie
             case 'n':
-                n();
+                root = n(&counter);
                 break;
             case 'v':
                 v();
